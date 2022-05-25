ba25ac4fbb17c7f621ef2ebebfd26b766992b365
dilumsadeepa <51749100+dilumsadeepa@users.noreply.github.com>
2022-05-25 17:06:16 +0530

    admin update

---
+++ TecMis/src/tecmis/Course.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tecmis;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Course {
+    
+    public String co_id;
+    public String coname;
+    public String depid;
+
+    public String getCo_id() {
+        return co_id;
+    }
+
+    public void setCo_id(String co_id) {
+        this.co_id = co_id;
+    }
+
+    public String getConame() {
+        return coname;
+    }
+
+    public void setConame(String coname) {
+        this.coname = coname;
+    }
+
+    public String getDepid() {
+        return depid;
+    }
+
+    public void setDepid(String depid) {
+        this.depid = depid;
+    }
+    
+}
