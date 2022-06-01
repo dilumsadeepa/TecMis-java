ab4d7eea2b93d3f0db8a85179cb80596cbc452b2
pimuditha <101864290+pimuditha@users.noreply.github.com>
2022-05-31 20:07:10 -0700

    edit profile

---
+++ TecMis/src/student/JoptionPane.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package student;
+
+/**
+ *
+ * @author Hp
+ */
+class JoptionPane {
+
+    static void showMessageDialog(EditProfile aThis, String plese_enter_the_student_id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
